96672a27ac99862ecca67295c2ffeabdba11678e
Arseni Rakutsko <Ars@Arsenis-MacBook-Pro.local>
2016-03-03 17:13:18 +0100

    create main function and 2variables

---
+++ example_1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main( int argc, const char * argv[] )
+{
+    int myInt, anotherInt;
+    myInt = 505;
+    anotherInt = myInt;
+    
+    cout << "Hello!" << endl;
+
+    return 0; 
+}
