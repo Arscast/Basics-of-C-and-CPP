bf517f757a7c1f6221563c4161f5608dc60033d1
Arseni Rakutsko <Ars@Arsenis-MacBook-Pro.local>
2016-03-04 15:34:55 +0100

    add postfix and prefix

--- example_1.c
+++ example_1.c
@@ -3,6 +3,9 @@ void SayHello( void );
 int main (int argc, const char * argv[])
 {
     SayHello();
+    
+    //
+    
     int myInt;
     
     myInt = 3 * 2;
@@ -21,11 +24,23 @@ int main (int argc, const char * argv[])
     printf( "myInt ---> %d\n", myInt );
     
     myInt /= 2;
+    printf( "myInt ---> %d\n\n\n", myInt );
+    
+    //
+    
+    
+    myInt = 5;
+    
+    myInt = myInt * 3;
     printf( "myInt ---> %d\n", myInt );
+    
+    printf( "myInt ---> %d\n", myInt++ );/* The value in the variable myInt increases ,after this the operator printf print this value*/
+    
+    printf( "myInt ---> %d\n", ++myInt );/*Operator printf print the value of the variable myInt and then increase this value by 1*/
     
     return 0;
 }
 void SayHello( void )
 {
-    printf( "Hello, world!\n" );
-} 
+    printf( "Hello, world!\n\n\n" );
+}
