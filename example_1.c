2d8f81e161dc30e1f087182853772f0cee5d7c2d
Arseni Rakutsko <Ars@Arsenis-MacBook-Pro.local>
2016-03-04 13:33:47 +0100

    adding operators in C

--- example_1.c
+++ example_1.c
@@ -2,11 +2,30 @@
 void SayHello( void );
 int main (int argc, const char * argv[])
 {
-  SayHello(); 
-
-  return 0;
- }
+    SayHello();
+    int myInt;
+    
+    myInt = 3 * 2;
+    printf("myInt ---> %d\n" , myInt);
+    
+    myInt += 1;
+    printf( "myInt ---> %d\n", myInt );
+    
+    myInt -= 5;
+    printf( "myInt ---> %d\n", myInt );
+    
+    myInt *= 10;
+    printf( "myInt ---> %d\n", myInt );
+    
+    myInt /= 4;
+    printf( "myInt ---> %d\n", myInt );
+    
+    myInt /= 2;
+    printf( "myInt ---> %d\n", myInt );
+    
+    return 0;
+}
 void SayHello( void )
 {
-  printf( "Hello, world!\n" );
+    printf( "Hello, world!\n" );
 } 
